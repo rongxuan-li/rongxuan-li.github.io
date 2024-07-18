41bd5a9260c14d6d2fc081188537f0ba25c433b9
Liu Tianchi <42459676+Liu-Tianchi@users.noreply.github.com>
2024-07-18 15:00:35 +0800

    Update main.py

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -4,22 +4,20 @@ import json
 from datetime import datetime
 import os
 
-author_id = '1W24GsQAAAAJ' 
-author = scholarly.search_author_id(author_id)
+author: dict = scholarly.search_author_id('1W24GsQAAAAJ')
 scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
+name = author['name']
 author['updated'] = str(datetime.now())
-author['publications'] = {v['author_pub_id']: v for v in author['publications']}
-
-
+author['publications'] = {v['author_pub_id']:v for v in author['publications']}
+print(json.dumps(author, indent=2))
 os.makedirs('results', exist_ok=True)
-with open(f'results/gs_data_{author_id}.json', 'w') as outfile:
+with open(f'results/gs_data.json', 'w') as outfile:
     json.dump(author, outfile, ensure_ascii=False)
-
 
 shieldio_data = {
   "schemaVersion": 1,
   "label": "citations",
   "message": f"{author['citedby']}",
 }
-with open(f'results/gs_data_shieldsio_{author_id}.json', 'w') as outfile:
+with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
     json.dump(shieldio_data, outfile, ensure_ascii=False)
